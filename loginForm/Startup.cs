7c115891290469e5ff0c07178dc6eec3eab0e1b7
aldianda <aldiandasilvayudasaputra@gmail.com>
2020-02-04 08:35:48 +0700

    read data. insert

---
+++ loginForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(loginForm.Startup))]
+namespace loginForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
