05a793dd828c587f1982c1bba2f23f90984b5089
aldianda <aldiandasilvayudasaputra@gmail.com>
2020-02-04 08:50:32 +0700

    read and insert

---
+++ ASPNETIDENTITYLOGIN/Controllers/RolesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using ASPNETIDENTITYLOGIN.Models;
+using System.Threading.Tasks;
+
+namespace ASPNETIDENTITYLOGIN.Controllers
+{
+    public class RolesController : Controller
+    {
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+        // GET: Roles
+        public ActionResult Index()
+        {
+            return View(GetDataRoles());
+        }
+        public async Task<ActionResult> GetDataRoles()
+        {
+            var result = await sqlConnection.QueryAsync<RoleVM>("EXEC SP_Retrieve_Role");
+            return Json(new { data = result }, JsonRequestBehavior.AllowGet);
+        }
+        public async Task<ActionResult> Create(RoleVM roleVM)
+        {
+            var affectedRows = await sqlConnection.ExecuteAsync("EXEC SP_InsertRole @Name", new { Name = roleVM.Name });
+            return Json(new { data = affectedRows }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
